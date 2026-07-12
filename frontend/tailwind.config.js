6fcb64a5a846f14ea8d56471f2a4bbe27cdc9c90
junnuahmad11-svg <junnuahmad11@gmail.com>
2026-07-12 16:31:40 +0530

    Create tailwind.config.js

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        dark: {
+          bg: '#0a0a0a',
+          panel: '#141414',
+          card: '#1a1a1a',
+          border: '#2a2a2a',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
